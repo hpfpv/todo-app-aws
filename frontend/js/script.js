5ca63ed0e34192674ca259b5b30a160f373baad7
H. Pierre-Francois <81161636+hpfpv@users.noreply.github.com>
2021-07-31 01:10:58 +0100

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -2,7 +2,7 @@ var todoApiEndpoint = 'https://j3cv37qhud.execute-api.us-east-1.amazonaws.com/de
 var todoFilesApiEndpoint = 'https://4oumdscha7.execute-api.us-east-1.amazonaws.com/dev/';
 var cognitoUserPoolId = 'us-east-1_fM3BzKm1u';
 var cognitoUserPoolClientId = '4ajb6clml9vft00cof689o6c0p';
-var bucketName = 'lambda.test.source';
+var bucketName = 'hpf-todo-app-files';
 var awsRegion = 'us-east-1';
 
 var gridScope;
